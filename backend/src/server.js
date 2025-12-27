7161bf84cf7c0a5cb5370e792e46dd70bfe4763a
Tailung2 <155643057+Tailung1@users.noreply.github.com>
2025-12-28 02:52:53 +0400

    Update cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://entertainment-web-coa17g0m0-tailung1s-projects.vercel.app/",
+      "https://entertainment-web-app-sandy-five.vercel.app/",
     ],
   })
 );
